0ea16a6f6fd1f3b21b7c0d9a052db4a8edc1fc8e
Marcel van den Elst <marcel@marutec.nl>
2012-06-28 13:19:18 +0200

    fixed setup

--- setup.py
+++ setup.py
@@ -1,22 +1,29 @@
-#!/usr/bin/env python
-from setuptools import setup
+import os
+
+from setuptools import setup, find_packages
+
+here = os.path.abspath(os.path.dirname(__file__))
+README = open(os.path.join(here, 'README.md')).read()
+
+requires = [
+    'mongoengine',
+    'bson',
+]
 
 setup(
     name = 'mongoengine-relational',
-    description = '''Mixin for MongoEngine that manages both sides of ToOne or ToMany relations''',
     version = 0.1,
+    description = '''Mixin for MongoEngine that manages both sides of ToOne or ToMany relations''',
+    long_description=README,
     author = 'Paul Uithol - Progressive Company',
     author_email = 'paul.uithol@progressivecompany.nl',
     url = 'http://github.com/ProgressiveCompany/mongoengine-relational',
-    packages=['mongoengine_privileges'],
-    requires=[
-        'mongoengine',
-        'bson',
-    ],
-    setup_requires=[
-        'mongoengine',
-        'bson',
-    ],
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    requires=requires,
+    install_requires=requires,
+    tests_require=requires,
     classifiers = [
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
